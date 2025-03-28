6349ffdb6583321a92c5788a2f2b1b9c035f8754
Anand Chaurasiya <128166047+anand-chau16@users.noreply.github.com>
2025-03-28 14:29:02 +0530

    final chat app

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyABInVO7Xj2cCEXiVlrTleY3fv864bK0NM",
+    authDomain: "chat-app-72b0d.firebaseapp.com",
+    projectId: "chat-app-72b0d",
+    storageBucket: "chat-app-72b0d.appspot.com",
+    messagingSenderId: "485034871935",
+    appId: "1:485034871935:web:579cb3ce9bf1b6f19fc34a"
+  };
